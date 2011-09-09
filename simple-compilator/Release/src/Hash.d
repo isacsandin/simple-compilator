db8641700a16ee8ca1bfb2ffe085aa9821f01f99
isacsandim@gmail.com <isacsandim@gmail.com@a41f0bd4-8df1-b404-f44b-6c78e1819bc5>
2011-09-09 18:36:19 +0000

    melhoras no automato...

--- simple-compilator/Release/src/Hash.d
+++ simple-compilator/Release/src/Hash.d
@@ -1,5 +1,8 @@
-src/Hash.d src/Hash.o: ../src/Hash.cpp ../src/Hash.hpp ../src/Utils.hpp
+src/Hash.d src/Hash.o: ../src/Hash.cpp ../src/Hash.hpp ../src/Utils.hpp \
+ ../src/Constantes.hpp
 
 ../src/Hash.hpp:
 
 ../src/Utils.hpp:
+
+../src/Constantes.hpp:
