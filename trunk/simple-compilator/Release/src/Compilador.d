6fe7f14d132fc8941f4a51e9ed50da00aa4c24c3
leandrodvmg@gmail.com <leandrodvmg@gmail.com@a41f0bd4-8df1-b404-f44b-6c78e1819bc5>
2011-10-29 11:53:11 +0000

    Tentativa de consertar o erro das variaveis globais. Uso de EXTERN.

--- trunk/simple-compilator/Release/src/Compilador.d
+++ trunk/simple-compilator/Release/src/Compilador.d
@@ -1,6 +1,8 @@
-src/Compilador.d src/Compilador.o: ../src/Compilador.cpp ../src/Hash.hpp \
- ../src/Utils.hpp ../src/LexAnalyzer.hpp ../src/Constantes.hpp \
- ../src/Globals.hpp ../src/Compilador.hpp
+src/Compilador.d src/Compilador.o: ../src/Compilador.cpp \
+ ../src/Compilador.hpp ../src/Hash.hpp ../src/Utils.hpp \
+ ../src/LexAnalyzer.hpp ../src/Constantes.hpp ../src/Globals.hpp
+
+../src/Compilador.hpp:
 
 ../src/Hash.hpp:
 
@@ -11,5 +13,3 @@ src/Compilador.d src/Compilador.o: ../src/Compilador.cpp ../src/Hash.hpp \
 ../src/Constantes.hpp:
 
 ../src/Globals.hpp:
-
-../src/Compilador.hpp:
