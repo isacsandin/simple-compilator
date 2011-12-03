a0139ea0b57abea41de5d124d6801126fd23a7c0
isacsandim@gmail.com <isacsandim@gmail.com@a41f0bd4-8df1-b404-f44b-6c78e1819bc5>
2011-12-03 15:03:57 +0000

    sintatico completo (sem regras semanticas)

---
+++ simple-compilator-fase2/Debug/src/LexAnalyser.d
@@ -0,0 +1,11 @@
+src/LexAnalyser.d src/LexAnalyser.o: ../src/LexAnalyser.cpp \
+ ../src/LexAnalyzer.hpp ../src/Hash.hpp ../src/Constantes.hpp \
+ ../src/Globals.hpp
+
+../src/LexAnalyzer.hpp:
+
+../src/Hash.hpp:
+
+../src/Constantes.hpp:
+
+../src/Globals.hpp:
